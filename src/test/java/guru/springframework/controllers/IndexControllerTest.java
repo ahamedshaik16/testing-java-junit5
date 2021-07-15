e5206d9b8ebd406b677f0d7a8edc2ba86b453684
ahamedshaik16 <ahamedshaik16@gmail.com>
2021-07-15 13:50:14 +0530

    Conditional JUnit Test execution

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
 
 import java.time.Duration;
 
@@ -60,4 +61,35 @@ class IndexControllerTest {
     void testAssumptionTureAssumptionIsTrue() {
         assumeTrue("SOHAIL".equalsIgnoreCase("SOHAIL"));
     }
+
+    @EnabledOnOs(OS.MAC)
+    @Test
+    void testMeOnMacOS() {
+
+    }
+
+    @EnabledOnOs(OS.WINDOWS)
+    @Test
+    void testMeOnWindows() {
+
+    }
+
+    @EnabledOnJre(JRE.JAVA_8)
+    @Test
+    void testMeOnJava8() {
+
+    }
+
+    @EnabledOnJre(JRE.JAVA_11)
+    @Test
+    void testMeOnJava11() {
+
+    }
+
+    @EnabledIfEnvironmentVariable(named =  "USER", matches = "sohail")
+    @Test
+    void testIfUserSohail() {
+
+    }
+
 }
