63aa355ec8616bdb842d6fc4ecbfac9ac83821b2
ahamedshaik16 <ahamedshaik16@gmail.com>
2021-07-15 12:27:09 +0530

    Dependent Assertions

---
+++ src/test/java/guru/springframework/model/OwnerTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+    @Test
+    void dependentAssertions() {
+        Owner owner = new Owner(11l, "Sohail", "Ahamed");
+        owner.setCity("Hyderabad");
+        owner.setTelephone("9876543210");
+
+        assertAll("Properties Test",
+                () -> assertAll("Person Properties",
+                        () -> assertEquals("Sohail", owner.getFirstName()),
+                        () -> assertEquals("Ahamed", owner.getLastName())),
+                () -> assertAll("Owner Properties",
+                        () -> assertEquals("Hyderabad", owner.getCity()),
+                        () -> assertEquals("9876543210", owner.getTelephone())));
+    }
+
+}
