001d544b5c0c4744d96dd518e7d69002d4eb9684
ahamedshaik16 <ahamedshaik16@gmail.com>
2021-07-16 12:21:08 +0530

    Parameterized CSV source

--- src/test/java/guru/springframework/model/OwnerTest.java
+++ src/test/java/guru/springframework/model/OwnerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -45,5 +46,16 @@ class OwnerTest implements ModelTests {
     @EnumSource(OwnerType.class)
     void enumTest(OwnerType ownerType) {
         System.out.println(ownerType);
+    }
+
+    @DisplayName("CSV Input Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {argumentsWithNames}")
+    @CsvSource({
+            "HX, 1, 1",
+            "SC, 2, 2",
+            "OH, 3, 4"
+    })
+    void csvInputTest(String stateName, int val1, int val2) {
+        System.out.println(stateName + " = " + val1 + ":" + val2);
     }
 }
